cc9f74805b0ea65901a38819c63a5e9b09ced7cb
ennbou <ennbou@yandex.com>
2023-08-29 19:51:27 +0100

    feat(add zendesk sdk): use zendeskMobileSdkRN in example project

---
+++ example/src/ZendeskConfig.ts
@@ -0,0 +1,7 @@
+const config = {
+  zendeskUrl: 'https://Your_Demain_Name.........zendesk.com',
+  appId: 'base64...............',
+  clientId: 'mobile_sdk_client_.............',
+};
+
+export default config;
