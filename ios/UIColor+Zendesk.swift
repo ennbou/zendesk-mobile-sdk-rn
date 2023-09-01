6f4ebff0d9a31ea5135d320a5e3eba8372ff1b71
ennbou <ennbou@yandex.com>
2023-09-01 08:56:00 +0100

    fix: fix init UIColor with colorHexFormat for iOS

--- ios/UIColor+Zendesk.swift
+++ ios/UIColor+Zendesk.swift
@@ -13,7 +13,7 @@ extension UIColor {
     }else{
       var rgbValue:UInt64 = 0
       Scanner(string: colorHexFormat).scanHexInt64(&rgbValue)
-      self.init(red: CGFloat((rgbValue & 0xFF0000) >> 16),green: CGFloat((rgbValue & 0x00FF00) >> 8),blue: CGFloat(rgbValue & 0x0000FF),alpha: 1)
+      self.init(red: CGFloat((rgbValue & 0xFF0000) >> 16)/255,green: CGFloat((rgbValue & 0x00FF00) >> 8)/255,blue: CGFloat(rgbValue & 0x0000FF)/255,alpha: 1)
     }
   }
   
